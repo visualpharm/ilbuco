a0d3e7af735453d071ba7e16e67ab8ce7e1a09bb
Ivan Braun <ivan@generated.photos>
2025-05-16 20:10:33 -0300

    fixed the build script

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 
+// This is a static export version that works with `next export`
 export default function Home() {
+  // For static exports, we'll use a meta refresh tag
   return (
     <div className="container">
       <Head>
-        <title>Il Buco</title>
+        <title>Il Buco - Redirecting</title>
         <meta name="description" content="Il Buco - Restaurant" />
         <link rel="icon" href="/favicon.ico" />
+        <meta httpEquiv="refresh" content={`0;url=/rooms/index.html`} />
       </Head>
-
       <main>
-        <h1>Welcome to Il Buco</h1>
-        <p>Website coming soon...</p>
+        <h1>Redirecting to rooms...</h1>
+        <p>If you are not redirected automatically, <a href="/rooms/index.html">click here</a>.</p>
       </main>
-
       <style jsx global>{`
         body {
           margin: 0;
